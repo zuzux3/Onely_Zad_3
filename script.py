ad0bb7618a10c007199a744442e90710d709cf89
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2021-12-29 21:58:51 +0100

    making myself remember

--- script.py
+++ script.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
 
 data = pd.read_csv("indexing.csv")
 
-domainDescription = data['domain'].describe()
-print(domainDescription)
