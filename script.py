ab51f1ffd20680957793e9f4c1f9d490a4566a2e
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2021-12-29 21:37:55 +0100

    I just haven't seen mispelling

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
-import matlplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("indexing.csv")
 
-domain = data["domain"].value_counts()
+domainDescription = data['domain'].describe()
+print(domainDescription)
