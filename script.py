087f11ca39de9e5bd5ff4c92f4d5cc1aa97b6712
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2021-12-29 22:42:49 +0100

    analyzing
    making status and status_details counting and analysis. showing graphically

--- script.py
+++ script.py
@@ -3,5 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 
-data = pd.read_csv("indexing.csv")
+fname = cbook.get_sample_data('indexing.csv', asfileobj=False)
+with cbook.get_sample_data('G:\Onely_Zad_3\indexing.csv') as file:
+    data = pd.read_csv("indexing.csv")
 
+status = pd.DataFrame(data["status"].value_counts())
+print(status)
+status.plot(kind='bar')
+
+plt.show()
+
+status_details = pd.DataFrame(data['status_details'].value_counts())
+print(status_details)
+status_details.plot(kind='bar')
+
+plt.show()
