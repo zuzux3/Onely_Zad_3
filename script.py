5383d3735e03113de69eae8c8eec0f78ad484ba6
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2022-01-06 20:22:56 +0100

    adding more plots and heatmap

--- script.py
+++ script.py
@@ -1,21 +1,34 @@
+import numpy
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
+import seaborn as sns
 
 fname = cbook.get_sample_data('indexing.csv', asfileobj=False)
 with cbook.get_sample_data('G:\Onely_Zad_3\indexing.csv') as file:
     data = pd.read_csv("indexing.csv")
 
+date = pd.DataFrame(data['date'].value_counts())
+print(date)
+date.plot(kind = 'bar')
+
+plt.show()
+
+domain = pd.DataFrame(data['domain'].value_counts())
+print(domain)
+domain.plot(kind = 'bar', figsize = (15, 7))
+
+plt.show()
+
 status = pd.DataFrame(data["status"].value_counts())
 print(status)
-status.plot(kind='bar')
-
+status.plot(kind='bar', figsize = (6, 7))
+ 
 plt.show()
 
 status_details = pd.DataFrame(data['status_details'].value_counts())
 print(status_details)
-status_details.plot(kind='bar')
+status_details.plot(kind='bar', figsize = (7, 10))
 
 plt.show()
 
@@ -27,3 +40,18 @@ data['pages'] = data['pages'].astype('category').cat.codes
 dataCorr = data.corr()
 
 print(dataCorr)
+
+ax = sns.heatmap(
+    dataCorr,
+    vmin = -1, vmax = 1, center = 0,
+    cmap = sns.diverging_palette(20, 220, n = 200),
+    square = True
+)
+
+ax.set_xticklabels(
+    ax.get_xticklabels(),
+    rotation = 45,
+    horizontalalignment='right'
+)
+
+plt.show()
